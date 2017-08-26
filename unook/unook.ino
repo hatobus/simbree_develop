b02528949e87bc420487011d0ad1095659da19be
F_Haga <s1240056@gmail.com>
2017-08-25 20:30:05 -0700

    add TODO

--- unook/unook.ino
+++ unook/unook.ino
@@ -14,24 +14,28 @@ int distance;
 void setup() {
 //pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
 //pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-Serial.begin(9600); // Starts the serial communication
+Serial.begin(115200); // Starts the serial communication
 }
 void loop() {
 pinMode(trigPin,OUTPUT);
 // Clears the trigPin
 digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
+delayMicroseconds(trigPin);
 // Sets the trigPin on HIGH state for 10 micro seconds
 digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
+Serial.println(analogRead(echoPin));
+//delayMicroseconds(10);
 digitalWrite(trigPin, LOW);
 // Reads the echoPin, returns the sound wave travel time in microseconds
 pinMode(echoPin,INPUT);
 duration = pulseIn(echoPin, HIGH);
+Serial.println(analogRead(4));
 // Calculating the distance
 distance= duration*0.034/2;
 // Prints the distance on the Serial Monitor
 Serial.print("Distance: ");
 Serial.println(distance);
-delay(100);
+delay(1);
 }
+
+//TODO simbleeの距離0のナゾを究明
