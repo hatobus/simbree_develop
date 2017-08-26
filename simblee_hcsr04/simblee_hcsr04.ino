fef016603229bda88b080f1feae472870ef5a7c1
F_Haga <s1240056@gmail.com>
2017-08-25 17:51:23 -0700

    uno で動くようになった

--- simblee_hcsr04/simblee_hcsr04.ino
+++ simblee_hcsr04/simblee_hcsr04.ino
@@ -1,11 +1,11 @@
 // HC-SR04を使えるようにする
 // スマホで距離を見れるように．
-//  #define PING_PIN 4
-//  #include <NewPing.h>      
+  #define PING_PIN 4
+  #include <NewPing.h>     
 
 // †おまじない†
 
-//  NewPing sonar(PING_PIN, PING_PIN);
+  NewPing sonar(PING_PIN, PING_PIN);
 const int pingPIN = 4;
 
 void setup() {
@@ -21,33 +21,33 @@ void setup() {
 
 void loop() {
    long dura,inch,cm;
-  // put your main code here, to run repeatedly:
+//   put your main code here, to run repeatedly:
 
-//  delay(50);
-//  int uS = sonar.ping();
-//  // Send ping, get ping time in microseconds (uS).
+  delay(50);
+  int uS = sonar.ping();
+  // Send ping, get ping time in microseconds (uS).
+
+  Serial.print("Ping: ");
+  Serial.print(uS);
+  
+  Serial.println("cm");
+
+
+//  pinMode(pingPIN,OUTPUT);
+//  digitalWrite(pingPIN,LOW);
+//  delay(2);
+//  digitalWrite(pingPIN,HIGH);
+//  delay(5);
+//  digitalWrite(pingPIN,LOW);
 //
-//  Serial.print("Ping: ");
-//  Serial.print(uS);
-//  
-//  Serial.println("cm");
-
-
-  pinMode(pingPIN,OUTPUT);
-  digitalWrite(pingPIN,LOW);
-  delay(2);
-  digitalWrite(pingPIN,HIGH);
-  delay(5);
-  digitalWrite(pingPIN,LOW);
-
-  pinMode(pingPIN, INPUT);
-  dura = pulseIn(pingPIN, HIGH);
- 
-  Serial.print(inch);
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
+//  pinMode(pingPIN, INPUT);
+//  dura = pulseIn(pingPIN, HIGH);
+// 
+//  Serial.print(inch);
+//  Serial.print("in, ");
+//  Serial.print(cm);
+//  Serial.print("cm");
+//  Serial.println();
  
   delay(100);
 
