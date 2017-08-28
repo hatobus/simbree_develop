6334229e035ee276b3188fd9585eaa32553e0b1c
F_Haga <s1240056@gmail.com>
2017-08-28 07:29:58 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# simbree_develop
+# simblee develop
+
+スペルミスしているのはまあ許して 😇
+(I'm sorry for my typing mistakes 😇)
+
+このレポジトリはsimbleeで，測距センサーHC-SR04を使用するためのレポジトリになります．使用が可能になったところで，このレポジトリをそれっぽい名前に変更しておきます．
+(This repo is enable to use distance sensor "HC-SR04". If it can use it, this repo to rename for that.)
+
+## 結線図 (Circle example)
+![kairozu](./img/hcsr04.png)
+
+### 注意点
+
+HC-SR04のTrugピンとEchoピンを1k$\Omega$の抵抗でショートさせている．これで1ピンで操作が可能なので，VCCとGND，信号線が一つしかないsimbleeで測距が可能になる．simbleeの時にはプルアップとして10k$\Omega$くらいの抵抗を入れておく必要がある．
--- unook/unook.ino
+++ unook/unook.ino
@@ -1,41 +1,51 @@
 /*
-* Ultrasonic Sensor HC-SR04 and Arduino Tutorial
-*
-* Crated by Dejan Nedelkovski,
-* www.HowToMechatronics.com
-*
+こっちでdelayを使わない方に書き換える
+2017/08/27 in US
 */
-// defines pins numbers
-const int trigPin = 9;
-const int echoPin = 9;
-// defines variables
-long duration;
-int distance;
+  // defines pins numbers
+  const int trigPin = 9;
+  const int echoPin = 9;
+  const int wait = 10;
+  // defines variables
+  long duration;
+  int distance;
+
 void setup() {
-//pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-//pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-Serial.begin(115200); // Starts the serial communication
+  
+  //pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  //pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(115200); // Starts the serial communication
+
 }
 void loop() {
-pinMode(trigPin,OUTPUT);
-// Clears the trigPin
-digitalWrite(trigPin, LOW);
-delayMicroseconds(trigPin);
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
-Serial.println(analogRead(echoPin));
-//delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-pinMode(echoPin,INPUT);
-duration = pulseIn(echoPin, HIGH);
-Serial.println(analogRead(4));
-// Calculating the distance
-distance= duration*0.034/2;
-// Prints the distance on the Serial Monitor
-Serial.print("Distance: ");
-Serial.println(distance);
-delay(1);
+
+  pinMode(trigPin,OUTPUT);
+  // Clears the trigPin
+  
+  digitalWrite(trigPin, LOW);
+  
+  delayMicroseconds(trigPin);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  
+  Serial.println(analogRead(echoPin));
+  
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  pinMode(echoPin,INPUT);
+  
+  duration = pulseIn(echoPin, HIGH);
+
+  // 下はデバッグ用  
+  Serial.println(analogRead(4));
+  
+  // Calculating the distance
+  distance= duration*0.034/2;
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(1);
 }
 
 //TODO simbleeの距離0のナゾを究明
